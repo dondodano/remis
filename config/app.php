613a58c48a1b4c1f7128d8057da079eb352b52b3
Dondo Dano <oshawott.jr@gmail.com>
2024-01-05 14:17:43 +0800

    REMIS update on 01-05-2024 02:17 PM

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\LadderServiceProvider::class,
 
         Laravel\Socialite\SocialiteServiceProvider::class,
     ])->toArray(),
