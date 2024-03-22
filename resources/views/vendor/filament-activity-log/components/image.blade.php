55f8d0bbb98675ac39ac39f23001cb0132b15d91
Dondo Dano <oshawott.jr@gmail.com>
2024-03-22 15:36:05 +0800

    REMIS update on 03-22-2024 03:36 PM

--- resources/views/vendor/filament-activity-log/components/image.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="flex flex-wrap gap-2">
-    @foreach ((array) $value as $_value)
-        <x-filament::avatar
-            :src="$_value"
-            size="lg"
-            :class="$field->rounded === 'circle' ? 'rounded-full' : 'rounded-md'"
-        />
-    @endforeach
-</div>
